734e544eacffae7fb1f51ef66a5d35c13a947154
jeoung-youjin <150499204+jeoung-youjin@users.noreply.github.com>
2024-02-02 16:17:08 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@ var swiper = new Swiper(".mainContain", {
     },
   });
 
-
 var swiper = new Swiper(".subscription", {
   slidesPerView: 3,
   spaceBetween: 30,
@@ -112,117 +111,73 @@ eventBt.addEventListener("click",()=>{
 // main-visual-list
 
 // promotion 
-// json 으로 수정하기
-function ProList(link,days,desc){
-  this.link=link;
-  this.days=days;
-  this.desc=desc;
-};
-
-let list01 = new ProList("/portfolio02/img/pro-list01-01.jpg","2023-12-26 ~ 2023-12-30","12월 31DAY 사전예약 시 6,500원 OFF 혜택!")
-let list02 = new ProList("/portfolio02/img/pro-list01-02.jpg","2023-12-15 ~ 소진 시 까지","X-MAS 케이크 구매 시 허그미 조안 테디베어 11,900원!")
-let list03 = new ProList("/portfolio02/img/pro-list01-03.jpg","2023-12-15 ~ 2023-12-31","KT멤버쉽 달.달 혜택 파인트30% 할인혜택")
-let list04 = new ProList("/portfolio02/img/pro-list01-04.jpg","2023-12-08 ~ 소진 시 까지","X-MAS 케이크 구매 시 하트베어 테디백 8,900원!")
-let list05 = new ProList("/portfolio02/img/pro-list01-05.jpg","2023-12-01 ~ 소진 시 까지","쿼터 이상 구매 시 원더랜드 스노우볼 매장판매 9,900원!")
-let list06 = new ProList("/portfolio02/img/pro-list02-01.jpg","2023-11-24 ~ 소진 시 까지","쿼터 이상 구매 시 도라에몽 액션 피규어 10,900원")
-let list07 = new ProList("/portfolio02/img/pro-list02-02.jpg","2023-11-17 ~ 소진 시 까지","쿼터 이상 구매 시 도라에몬 미니 케리백 9,900원")
-let list08 = new ProList("/portfolio02/img/pro-list02-03.jpg","2023-11-08 ~ 소진 시 까지","파인트 이상 구매 시 도라에몽 여권케이스 3,900원 ")
-let list09 = new ProList("/portfolio02/img/pro-list02-04.jpg","상시운영","도라에몽의 하얀모찌 단팥죽 내부 취식+배달 & 픽업가능 매장안내")
-let list10 = new ProList("/portfolio02/img/pro-list02-05.jpg","상시운영","도라에몽의 하얀 모찌 단팥죽 배달 & 픽업 가능 매장안내")
-let list11 = new ProList("/portfolio02/img/pro-list03-01.jpg","2023-09-04 ~ 소진시까지","해피앱 스캔시 아이스크림 스쿱퍼 3천원 할인")
-let list12 = new ProList("/portfolio02/img/pro-list03-02.jpg","2023-07-03 ~ 소진시까지","모슈 텀블러 머그 MD 구매 시 배라 3종 쿠폰 증정")
-let list13 = new ProList("/portfolio02/img/pro-list03-03.jpg","2023-07-05 ~ 소진시까지","아이스크림 보냉 컨테이너 매장판매 시작!")
-let list14 = new ProList("/portfolio02/img/pro-list03-04.jpg","상시 운영","SKT 우주패스, 배스킨라빈스 구독 시 7천원 OFF!")
-let list15 = new ProList("/portfolio02/img/pro-list03-05.png","상시 운영","1회용 컵 사용 줄이기 안내")
-
-// list01
-let lists=[list01,list02,list03,list04,list05]
 let pageOne = document.querySelector(".pro-slide-list01");
-console.log(pageOne)
-
-let liGroup = '';
-for (const list of lists) {
-  liGroup += `
-    <li>
-      <a href="#">
-        <div class="img-frame">
-          <img src="${list.link}" alt="${list.days},${list.desc}">
-        </div>
-        <span>${list.days}</span>
-        <p>${list.desc}</p>
-      </a>
-    </li>
-    `;
-}
-pageOne.innerHTML = liGroup;
-
-// list02
-let secLists=[list06,list07,list08,list09,list10]
 let pageTw = document.querySelector(".pro-slide-list02");
-let secGroup = '';
-for (const secList of secLists) {
-  secGroup += `
-    <li>
-      <a href="#">
-        <div class="img-frame">
-          <img src="${secList.link}" alt="">
-        </div>
-        <span>${secList.days}</span>
-        <p>${secList.desc}</p>
-      </a>
-    </li>
-    `;
-}
-pageTw.innerHTML = secGroup;
-
-// list03
-let trLists=[list11,list12,list13,list14,list15]
 let pageTr = document.querySelector(".pro-slide-list03");
+let liGroup = '';
+let secGroup = '';
 let trGroup = '';
-for (const trList of trLists) {
-  trGroup += `
-    <li>
-      <a href="#">
-        <div class="img-frame">
-          <img src="${trList.link}" alt="">
-        </div>
-        <span>${trList.days}</span>
-        <p>${trList.desc}</p>
-      </a>
-    </li>
-    `;
-}
-pageTr.innerHTML = trGroup;
 
-// recommend 탭 이벤트
-let recommendBt = document.querySelectorAll(".recommend-bt > p")
-console.log(recommendBt)
+fetch("../portfolio02/js/promotion.json")
+.then((response)=>response.json())
+.then((json)=>{
+  data=json.프로모션.슬라이드1
+  data.forEach(element => {
+    liGroup += 
+    makePageTemp(element)
+  });
+  pageOne.innerHTML=liGroup
 
-// br-benefit 리스트 
-let benefitList = document.querySelector(".benefitSwiper > ul")
-function Benefit(img,des){
-  this.img=img,
-  this.des=des
-} 
+  data=json.프로모션.슬라이드2;
+  data.forEach(element =>{
+    secGroup += 
+    makePageTemp(element)
+  })
+  pageTw.innerHTML=secGroup
 
-let benefit01 = new Benefit("/portfolio02/img/benefit-list01.jpg","T맵버십 고객 대상 레디팩 50% OFF 2023년 10월 04일부터 2023년 12월 31일까지")
-let benefit02 = new Benefit("/portfolio02/img/benefit-list02.jpg","베스킨라벤스의 다양한 제휴혜택 상시운영")
-let benefit03 = new Benefit("/portfolio02/img/benefit-list03.jpg","이제 베라에서도 애플페이를 만나보세요 상시운영")
-let benefit04 = new Benefit("/portfolio02/img/benefit-list04.jpg","오직 해피앱에서만 해피포인트 5% 적립 상시운영")
-let benefit05 = new Benefit("/portfolio02/img/benefit-list05.jpg","KT맴버십 특별한 제휴혜택 생일쿠폽,파인트 50% 할인, 블록팩 FREE 상시운영")
-let benefit06 = new Benefit("/portfolio02/img/benefit-list06.jpg","T맵버쉽 회원대상 싱글 레귤러 최대 50% 할인 또는 적립 2023년 02월 01일부터 2024년 01월 31일까지")
-let benefit07 = new Benefit("/portfolio02/img/benefit-list07.jpg","현대카드 M포인트 50% 사용 상시운영")
+  data=json.프로모션.슬라이드3
+  data.forEach(element =>{
+    trGroup += 
+    makePageTemp(element)
+  });
+  pageTr.innerHTML=trGroup
+})
 
-let beGroup =``
-let benefits =[benefit01,benefit02,benefit03,benefit04,benefit05,benefit06,benefit07]
 
-for(const benefit of benefits){
-  beGroup +=`
-  <li class="swiper-slide">
+//함수 적용
+function makePageTemp(element){
+  return`
+  <li>
     <a href="#">
-      <img src="${benefit.img}" alt="${benefit.des}">
+      <div class="img-frame">
+        <img src="${element.이미지}" alt="">
+      </div>
+      <span>${element.기간}</span>
+      <p>${element.설명}</p>
     </a>
   </li>
   `
 }
+
+
+// recommend 탭 이벤트
+let recommendBt = document.querySelectorAll(".recommend-bt > p")
+
+// br-benefit 리스트   
+let benefitList = document.querySelector(".benefitSwiper > ul")
+let beGroup =``
+fetch("../portfolio02/js/promotion.json")
+.then((response) => response.json())
+.then(json => {
+  data= json.제휴;
+  data.forEach(element =>{
+    beGroup +=  `
+  <li class="swiper-slide">
+    <a href="#">
+      <img src="${element.이미지}" alt="${element.정보}">
+    </a>
+  </li>
+  `
+})
 benefitList.innerHTML=beGroup
+})
--- js/sub2-1.js
+++ js/sub2-1.js
@@ -12,84 +12,46 @@ eventButtons.forEach((eventButton,index)=>{
 });
 eventButtons[0].click();
 
-// 프로모션 all List 
-let allEventList = document.querySelector(".event-tab-box > .all-event")
-console.log(allEventList)
-fetch("../js/sub2-1.json")//json파일 읽어오기
-    .then((response) => response.json())//읽어온 데이터를 json으로 변환
-    .then((json) => {
-      data = json.전체이벤트
-      let allList=``
-      data.forEach(element => {
-        allList+=` 
-      <li>
-        <a href="#">
-          <div class="event-img">
-            <img src="${element.img}" alt="">
-          </div>
-          <div class="event-des">
-            <span>${element.category}</span>
-            <p>${element.tit}</p>
-            <em>${element.date}</em>
-          </div>
-        </a>
-      </li>
-      `
-    });
-    allEventList.innerHTML = allList
-  
-    });
 
-
-
-// 프로모션 이벤트 리스트
 let mainEventList = document.querySelector(".event-tab-box > .promotion")
-fetch("../js/sub2-1.json")//json파일 읽어오기
-    .then((response) => response.json())//읽어온 데이터를 json으로 변환
-    .then((json) => {
-        data = json.프로모션;//json에 있는 items만 받아오기
-        let proList = '';
-        data.forEach(element => {
-            proList+=` 
-          <li>
-            <a href="#">
-              <div class="event-img">
-                <img src="${element.img}" alt="">
-              </div>
-              <div class="event-des">
-                <span>${element.category}</span>
-                <p>${element.tit}</p>
-                <em>${element.date}</em>
-              </div>
-            </a>
-          </li>
-          `
-        });
-        mainEventList.innerHTML=proList
-    });
-
-// 제휴 이벤트 리스트 
 let affiliateList = document.querySelector(".event-tab-box > .affiliate")
-  fetch("../js/sub2-1.json")
-    .then((response) => response.json())//읽어온 데이터를 json으로 변환
-    .then((json) => {
-        data = json.제휴혜택;//json에 있는 items만 받아오기
-        let affList = '';
-        data.forEach(element => {
-          affList+=` 
-          <li>
-            <a href="#">
-              <div class="event-img">
-                <img src="${element.img}" alt="">
-              </div>
-              <div class="event-des">
-                <span>${element.category}</span>
-                <p>${element.tit}</p>
-                <em>${element.date}</em>
-              </div>
-            </a>
-          </li>
-          `
-        });
-        affiliateList.innerHTML=affList
+
+fetch("../portfolio02/js/sub2-1.json")
+.then((response) => response.json())
+.then((json) => {
+  // 프로모션 이벤트 리스트
+  data = json.프로모션;
+  let proList = '';
+  data.forEach(element => {
+    proList+=
+    makeEventTemp(element);
+  });
+  mainEventList.innerHTML=proList
+  
+  // 제휴 이벤트 리스트 
+    data = json.제휴혜택;
+    let affList = '';
+    data.forEach(element => {
+      affList+=
+      makeEventTemp(element);
+    });
+    affiliateList.innerHTML=affList
 });
+
+//함수 적용
+function makeEventTemp(element){
+  return`
+    <li>
+      <a href="#">
+        <div class="event-img">
+          <img src="${element.img}" alt="">
+        </div>
+        <div class="event-des">
+          <span>${element.category}</span>
+          <p>${element.tit}</p>
+          <em>${element.date}</em>
+        </div>
+      </a>
+   </li>
+  `;
+}
